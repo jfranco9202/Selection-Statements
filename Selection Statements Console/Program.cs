9ed6f6393c69410a185e2b008becc9b1017ec047
jfranco9202 <jeremy.franco41@yahoo.com>
2021-12-22 10:45:15 -0600

    Selection Statements

---
+++ Selection Statements Console/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Selection_Statements_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNumber = 100;
+
+            Console.WriteLine("Guess my number that is a common number that has 3 integers?");
+            int userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess < myNumber)
+            {
+                Console.WriteLine("Too Low!!!");
+            }
+
+            else if (userGuess > myNumber)
+            {
+                Console.WriteLine("Too High!!!");
+            }
+
+
+            else if (userGuess == myNumber)
+            {
+                Console.WriteLine("Great job you know my number!!");
+            }
+
+            else
+            {
+                Console.WriteLine("Nevermind");
+            }
+
+
+        }
+    }
+     
+    
+    
+}
